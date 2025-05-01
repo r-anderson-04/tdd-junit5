416c79e362f1c35ee894551634e70a73fd4a2a64
r-anderson-04 <githubburner0@gmail.com>
2025-05-01 03:45:36 +0000

    feat: isBuzz added

--- src/test/java/com/example/project/FizzBuzzTests.java
+++ src/test/java/com/example/project/FizzBuzzTests.java
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.framework.TestCase;
 
-
-public class FizzBuzzTests {
+public class FizzBuzzTests extends TestCase{
     
 
     @Test
     public void isFizzTest() throws Exception {
         FizzBuzz fb = new FizzBuzz();
         assertEquals(true, isFizz(3));
+    }
+
+    @Test
+    public void isBuzzTest() throws Exception {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals(true, isBuzz(5));
     }
     
 }
