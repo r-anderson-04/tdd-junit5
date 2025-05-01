28df72bbac7ce8f6ec85749ba6052406a47d697d
r-anderson-04 <githubburner0@gmail.com>
2025-05-01 04:00:56 +0000

    code: added main to FizzBuzz.java

--- src/main/java/com/example/project/FizzBuzz.java
+++ src/main/java/com/example/project/FizzBuzz.java
@@ -2,6 +2,8 @@ package com.example.project;
 
 public class FizzBuzz {
 
+    static FizzBuzz fb = new FizzBuzz();
+
     public boolean isFizz(int n) {
         if(n%3 == 0) return true;
         
@@ -18,6 +20,25 @@ public class FizzBuzz {
         if(isFizz(p) && isBuzz(p))  return true;
 
         return false;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++)
+        {
+            if (fb.isFizzBuzz(i))
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if (fb.isFizz(i)) {
+                System.out.println("Fizz");
+            } 
+            else if (fb.isBuzz(i)) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
     }
     
 }
--- src/test/java/com/example/project/FizzBuzzTests.java
+++ src/test/java/com/example/project/FizzBuzzTests.java
@@ -8,27 +8,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.framework.TestCase;
 
-public class FizzBuzzTests extends TestCase{
+public class FizzBuzzTests {
     
 
     @Test
     public void isFizzTest() throws Exception {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals(true, isFizz(3));
+        assertEquals(true, fb.isFizz(3));
     }
 
     @Test
     public void isBuzzTest() throws Exception {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals(true, isBuzz(5));
+        assertEquals(true, fb.isBuzz(5));
     }
 
     @Test
     public void isFizzBuzzTest() throws Exception {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals(true, 15);
+        assertEquals(true, fb.isFizzBuzz(15));
     }
     
 }
