678d865ea9aa5c3e92c97638762c71518cd776bc
r-anderson-04 <githubburner0@gmail.com>
2025-05-01 03:30:51 +0000

    feat: Added SetUp:

---
+++ src/test/java/com/example/project/FizzBuzzTests.java
@@ -0,0 +1,10 @@
+package com.example.project;
+
+public class FizzBuzzTests {
+    private FizzBuzz fb;
+
+    protected void setUP() throws Exception {
+        fb = new FizzBuzz();
+    }
+    
+}
